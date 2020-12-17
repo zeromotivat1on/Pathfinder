fa825a633a824ecb1983bf6d45e6ab6b3aeec5b2
Alexander Lyannoy <alyannoy@c1r3p3.khpi.ucode-connect.study>
2020-12-17 18:17:22 +0200

    changed

--- libmx/src/mx_strsplit.c
+++ libmx/src/mx_strsplit.c
@@ -2,24 +2,18 @@
 
 char **mx_strsplit(char const *s, char c) {
 	if (!s) return NULL;
-	int count_words = mx_count_words(s, c);
-	char **result = (char **)malloc((count_words + 1) * sizeof(char *));
+	int w_cnt = mx_count_words(s, c);
+	char **res = (char **)malloc((w_cnt + 1) * sizeof(char *));
 	int j = 0;
-	for (int i = 0; i < count_words; i++) {
-		int word_length = 0;
-		while (s[j] != c && s[j]) {
-			j++;
-			word_length++;
-		}
-
-		if(word_length == 0 && j < mx_strlen(s)){
-			j++;
-			i--;
-		} else {
-			char *tmp_word = mx_strndup(&s[j - word_length], word_length);
-			result[i] = tmp_word;
+	for (int i = 0; i < w_cnt; i++) {
+		int w_len = 0;
+		while (s[j] != c && s[j]) { j++; w_len++; }
+		if(w_len == 0 && j < mx_strlen(s)) { j++; i--; } 
+			else {
+			char *temp_word = mx_strndup(&s[j - w_len], w_len);
+			res[i] = temp_word;
 		}
 	}
-	result[count_words] = NULL;
-	return result;
+	res[w_cnt] = NULL;
+	return res;
 }
